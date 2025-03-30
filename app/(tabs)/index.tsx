bdac1572a81dcaee2191b73de5d947a3ef9e8d7c
aminokun <amine.dbh99@gmail.com>
2025-03-30 23:35:55 +0200

    just finished the circlebutton in chapter 4

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,6 +12,7 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,6 +24,7 @@ export default function Index() {
     if (!result.canceled) {
       console.log(result, "🙈");
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       console.log("You did not select any image. 😔");
     }
@@ -36,14 +38,21 @@ export default function Index() {
           selectedImage={selectedImage}
         />
       </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
